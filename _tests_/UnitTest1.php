5465b98bec9c3fd65eb0ad6e78768e1f74b48375
Nikita Kalitin <nikita.s.kalitin@gmail.com>
2019-10-24 11:51:58 +0300

    add unittest

---
+++ _tests_/UnitTest1.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * User: nikita.s.kalitin@gmail.com
+ * Date: 24.10.19
+ * Time: 11:32
+ */
+use PHPUnit\Framework\TestCase;
+use OdtHelper\xmlTreeSearch;
+
+class StackTest extends TestCase
+{
+    public function testclearFlatTree()
+    {
+        $testSrc = "<root> txt <tag1> txt2 <tag2 /><text:p2 abc4>Тигра [var8;block=tbs:<page:break />row;mag<text:p abx>net=row</text:p>]!</text:p2> txt3 <tag3 /><text:p>[var0;block=tbs:row;magnet=row]</text:p> </tag1> txt4  <tagx><tagxx><tagxxx><text:p>Кенгуру [var3;block=tbs:<page:break />row;mag<text:p abx>net=row</text:p>]!</text:p></tagxxx></tagxx></tagx><text:p>[var1;block=tbs:row;magnet=row]</text:p></root>";
+        $testModel = "<root> txt <tag1> txt2 <tag2 /><text:p2 abc4>Тигра [var8;block=tbs:row;magnet=row]!<page:break /><text:p abx></text:p></text:p2> txt3 <tag3 /><text:p>[var0;block=tbs:row;magnet=row]</text:p> </tag1> txt4  <tagx><tagxx><tagxxx><text:p>Кенгуру [var3;block=tbs:row;magnet=row]!<page:break /><text:p abx></text:p></text:p></tagxxx></tagxx></tagx><text:p>[var1;block=tbs:row;magnet=row]</text:p></root>";
+
+        $prs = new xmlTreeSearch($testSrc);
+
+        $prs->initTree();
+
+        $prs->clearFlatTree();
+
+        $this->assertSame($testModel, $prs->getResultString());
+
+    }
+}
--- san_pb2.php
+++ san_pb2.php
@@ -36,7 +36,6 @@ $zip = new clsTbsZip();
 $zip->Open($zipFile);
 
 $ok = $zip->FileExists($dataFile);
-//$tst1= "<root> txt <tag1> txt2 <tag2 /><text:p2 abc4>Тигра [var8;block=tbs:<page:break />row;mag<text:p abx>net=row</text:p>]!</text:p2> txt3 <tag3 /><text:p>[var0;block=tbs:row;magnet=row]</text:p> </tag1> txt4  <tagx><tagxx><tagxxx><text:p>Кенгуру [var3;block=tbs:<page:break />row;mag<text:p abx>net=row</text:p>]!</text:p></tagxxx></tagxx></tagx><text:p>[var1;block=tbs:row;magnet=row]</text:p></root>";
 
 if($ok){
     $xml = $zip->FileRead($dataFile);
